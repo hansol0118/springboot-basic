d9c039b02cd32d26180187684c2a919991176797
hansol0118 <wall197944@naver.com>
2026-02-23 10:48:57 +0900

    SQL 로그 상세 출력 세팅 - 2

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -16,9 +16,15 @@ public class BaseInitData {
     ApplicationRunner initDataRunner() {
         return args -> {
 
+//            if(postRepository.count() >0){
+//                return;
+//            }
+
+            System.out.println("초기 데이터를 로딩합니다.");
             Post post1 = new Post("제목1", "내용1");
             postRepository.save(post1);
-            System.out.println("초기 데이터를 로딩합니다.");
+            postRepository.findById(1);
+            // select * from post where id = 1;
         };
     }
 }
