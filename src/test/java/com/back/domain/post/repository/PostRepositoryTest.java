37d4f71652f8cd2add498c173315d0420a4beb06
hansol0118 <wall197944@naver.com>
2026-02-24 11:20:00 +0900

    37 38 39 40 41 test 환경 구축(롤백 기능, 메모리 DB 사용)

--- src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
+++ src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
@@ -5,22 +5,45 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
+@ActiveProfiles("test")
 public class PostRepositoryTest {
-        @Autowired
-        private PostRepository postRepository;
+    @Autowired
+    private PostRepository postRepository;
 
-        @Test
-        @DisplayName("2번 글 조회")
-        void t1() {
-            Post post1 = postRepository.findById(2).get();
+    @Test
+    @DisplayName("2번 글 조회")
+    void t1() {
+        Post post1 = postRepository.findById(2).get();
 
-            assertThat(post1.getId()).isEqualTo(2);
-            assertThat(post1.getTitle()).isEqualTo("제목2");
-            assertThat(post1.getContent()).isEqualTo("내용2");
+        assertThat(post1.getId()).isEqualTo(2);
+        assertThat(post1.getTitle()).isEqualTo("제목2");
+        assertThat(post1.getContent()).isEqualTo("내용2");
 
-        }
+    }
+
+    @Test
+    @DisplayName("글 생성")
+    void t2() {
+        Post post = new Post("제목3","내용3");
+        Post savePost = postRepository.save(post);
+        assertThat(post.getId()).isEqualTo(3);
+        assertThat(post.getTitle()).isEqualTo("제목3");
+        assertThat(post.getContent()).isEqualTo("내용3");
+
+    }
+
+    @Test
+    @DisplayName("글 개수 조회")
+    void t3() {
+        long cnt = postRepository.count();
+        assertThat(cnt).isEqualTo(2);
+    }
+
 }
